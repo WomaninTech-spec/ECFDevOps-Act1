d5a92366fa6f2e4750aece09bc48f876a556a855
WomaninTech-spec <83292442+WomaninTech-spec@users.noreply.github.com>
2024-02-12 14:06:22 +0100

    Add files via upload

--- main.tf
+++ main.tf
@@ -12,17 +12,49 @@ provider "aws" {
 }
 
 resource "aws_instance" "my-first-server" {
-  ami = "ami-0c7217cdde317cfec"
+  ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld!"
   }
 }
 
+resource "aws_instance" "python_app_instance" {
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+  key_name      = "key-0394ad6df7a5ea8f2"
+  
+  tags = {
+    Name = "HelloWorld!"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/your-key.pem")
+    host        = aws_instance.my-first-server.public_ip
+    timeout     = "2m"
+  }
+
+  provisioner "file" {
+    source      = "app.py"
+    destination = "/home/ec2-user/app.py"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install python3 -y",
+      "sudo pip3 install pyspark",
+      "python3 /home/ec2-user/app.py"
+    ]
+  }
+}
+
 resource "aws_emr_cluster" "spark_cluster" {
   name = var.cluster_name
   release_label = var.release_label 
-  applications  = var.applications
+  applications = var.applications
   
   ec2_attributes {
     subnet_id = var.subnet_id
@@ -32,28 +64,28 @@ resource "aws_emr_cluster" "spark_cluster" {
     key_name = var.key_name
   }
 
-  service_role     = var.service_role
+  service_role = var.service_role
   autoscaling_role = var.autoscaling_role
 
   master_instance_group {
-    instance_type  = var.master_instance_type
+    instance_type = var.master_instance_type
     instance_count = var.master_instance_count
   }
 
   core_instance_group {
-    instance_type  = var.core_instance_type
+    instance_type = var.core_instance_type
     instance_count = var.core_instance_count
   }
 }
 
 #Create the MongoDB Cluster with Document DB
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = var.cluster_identifier
-  engine                  = "docdb"
-  engine_version          = var.engine_version
-  master_username         = var.master_username
-  master_password         = var.master_password
-  enabled_cloudwatch_logs_exports = ["audit", "profiler"]
+  resource "aws_docdb_cluster" "docdb" {
+    cluster_identifier = var.cluster_identifier
+    engine = "docdb"
+    engine_version = var.engine_version
+    master_username = var.master_username
+    master_password = var.master_password
+    enabled_cloudwatch_logs_exports = ["audit", "profiler"]
 }
 
 #Create Instances in the previous Cluster 
